8583ffed08d3418494063bfd4be84768935afe9f
Anastasia Timoshenko <timoshenko927@gmail.com>
2018-05-27 19:02:16 +0700

    Number Guesser fixed.+Enigma

--- Timoshenko.Nsudotnet.NumberGuesser/Program.cs
+++ Timoshenko.Nsudotnet.NumberGuesser/Program.cs
@@ -7,12 +7,13 @@ namespace Timoshenko.Nsudetnet.NumberGuesser
     {
         static void Main(string[] args)
         {
-
+            
             Console.WriteLine("Hi!Please,write your name:");
             string name = Console.ReadLine();
 
             Random rand = new Random();
-            int number = rand.Next(100);
+            int number =  rand.Next(100);
+            
 
             string[] looserstring = { "Oh, dear {0},you are looser.",
                                  "{0}, maybe not today. {0}, maybe not this year.",
@@ -22,10 +23,13 @@ namespace Timoshenko.Nsudetnet.NumberGuesser
             List<string> experienceList = new List<string>();
 
             DateTime start = DateTime.Now;
+           
+
             while (true)
             {
                 Console.WriteLine("Let's play one game.You chould guess the number in my mind.Write a number from 0 to 100:");
                 string line = Console.ReadLine();
+
                 if (line.Contains("q"))
                 {
                     Console.WriteLine("Bad player,i am sorry.Goodbye,dear {0}", name);
@@ -33,39 +37,47 @@ namespace Timoshenko.Nsudetnet.NumberGuesser
                     return;
                 }
 
-                            
-                 int chosenValue = int.Parse(line);
-               
-
-                if (chosenValue == number)
+                try
                 {
-                    TimeSpan spentTime = DateTime.Now - start;
-                    Console.WriteLine("OOOOOOOYYYEEEE.REALLY.I GUESSED IT.Congrats!", experienceList.Count);
-                    Console.WriteLine("Number of tryes: {0}", experienceList.Count);
-                    foreach (string s in experienceList)
-                          Console.WriteLine(s);
-                    
-                    Console.WriteLine("Spent time: {0}", spentTime);
+                    int chosenValue = int.Parse(line);
+
+                    if (chosenValue == number)
+                    {
+                        Console.WriteLine("OOOOOOOYYYEEEE.REALLY.I GUESSED IT.Congrats!", experienceList.Count);
+                        Console.WriteLine("Number of tryes: {0}", experienceList.Count);
+                        foreach (string s in experienceList)
+                            Console.WriteLine(s);
+                        TimeSpan spentTime = DateTime.Now - start;
+                        Console.WriteLine("Spent time in minuts: {0}", (int)spentTime.TotalMinutes);
+                        Console.ReadLine();
+                        return;
+                    }
+                    else if (chosenValue < number)
+                    {
+                        Console.WriteLine("{0} < guess number", chosenValue);
+                        experienceList.Add(String.Format(" {0} < {1}", chosenValue, number));
+                    }
+                    else if (chosenValue > number)
+                    {
+                        Console.WriteLine("{0} > guess number", chosenValue);
+                        experienceList.Add(String.Format(" {0} > {1}", chosenValue, number));
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("There is not a number or a symbol q.");
                     Console.ReadLine();
                     return;
                 }
 
-                if (chosenValue < number)
-                {
-                    Console.WriteLine("{0} < guess number", chosenValue);
-                    experienceList.Add(String.Format(" {0} < {1}", chosenValue, number));
-                }
-                else
-                {
-                    Console.WriteLine("{0} > guess number", chosenValue);
-                    experienceList.Add(String.Format(" {0} > {1}", chosenValue, number));
-                }
 
                 if (experienceList.Count % 4 == 0)
+                {
                     Console.WriteLine(looserstring[rand.Next(looserstring.Length)], name);
-                
-            }
+                    Console.ReadLine();
+                }
 
+                }
         }
     }
 }
